e6f1e1fa963b5f05ea1e0f9beecc8a6fe6b40fda
Carol McMasters-Stone <cbeck@ucdavis.edu>
2018-04-10 21:19:15 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.0'
+
+setup(name='dssweb.views',
+      version=version,
+      description="misc views for the dssweb project",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Carol McMasters-Stone',
+      author_email='cbeck@ucdavis.edu',
+      url='https://github.com/CMcStone/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['dssweb'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          "plone.app.collection",
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )

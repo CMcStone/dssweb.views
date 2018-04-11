f3b0fbcfdac0ae6c6bb31319b437cf622b69a94e
Carol McMasters-Stone <cbeck@ucdavis.edu>
2018-04-11 10:15:14 -0700

    create install profile

---
+++ dssweb/views/__init__.py
@@ -0,0 +1,6 @@
+from zope.i18nmessageid import MessageFactory
+ExpandCollectionMessageFactory = MessageFactory('dssweb.views')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
